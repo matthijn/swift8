8fa0dd3dfdd837c77ca5c1f789a86b7659f17495
Matthijn Dijkstra <matthijndijkstra@gmail.com>
2015-11-12 16:43:42 +0100

    Improved keyboard mapping

--- README.md
+++ README.md
@@ -8,5 +8,16 @@ The technical document used writing the system can be found [here](http://devern
 
 Multiple games have been written for the CHIP-8 platform and are in the public domain. A number of these can be found on [DopeRoms](http://www.doperoms.com/roms/Chip-8.html). These include Space Invaders, Pong, Tetris and more. A number of these ROMS can be found in the roms folder.
 
+## Keys
+
+The original systems used an [http://laurencescotford.co.uk/wp-content/uploads/2013/08/COSMAC-VIP-keypad-layout.gif]Hexidecimal Keyboard[/url]. The keys for the emulator are mapped to
+
+```
+1234
+qwer
+asdf
+zxcv
+```
+
 ## Remarks
 This is my first attempt to emulate any system, for now I am still working on it. And hopefully one day it will actually be able to run any of the fore-mentioned ROMS.
--- Swift8/Keyboard.swift
+++ Swift8/Keyboard.swift
@@ -13,22 +13,22 @@ class Keyboard
     
     // Mapping ASCII keycodes to the Chip8 key codes
     let mapping : [UInt8: Int8] = [
-        19: 0x1, // 1
-        20: 0x2, // 2
-        21: 0x3, // 3
-        22: 0x4, // 4
-        23: 0x5, // 5
-        24: 0x6, // 6
-        25: 0x7, // 7
-        26: 0x8, // 8
-        27: 0x9, // 9
-        28: 0x0, // 0
-        0:  0xA, // a
-        11: 0xB, // b
-        8:  0xC, // c
-        2:  0xD, // d
-        14: 0xE, // e
-        3:  0xF, // f
+        18: 0x1, // 1
+        19: 0x2, // 2
+        20: 0x3, // 3
+        21: 0x4, // 4
+        12: 0x5, // q
+        13: 0x6, // w
+        14: 0x7, // e
+        15: 0x8, // r
+        0: 0x9, // a
+        1: 0x0, // s
+        2:  0xA, // d
+        3: 0xB, // f
+        6:  0xC, // z
+        7:  0xD, // x
+        8: 0xE, // c
+        9:  0xF, // v
     ]
     
     var currentKey : Int8 = -1
